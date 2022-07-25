e1b117ceaa1f037d128958fe7993f97b4523ea22
ranyery <ranyerysc@gmail.com>
2022-07-25 19:33:21 -0300

    refactor: remove responsabilidades de lidar com dados na controller de produtos passa para a service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { ProdutosController } from './controllers/produtos.controller';
+import { ProdutosService } from './services/produtos.service';
 
 @Module({
   imports: [],
   controllers: [AppController, ProdutosController],
-  providers: [AppService],
+  providers: [AppService, ProdutosService],
 })
 export class AppModule {}
--- src/controllers/produtos.controller.ts
+++ src/controllers/produtos.controller.ts
@@ -7,41 +7,39 @@ import {
   Delete,
   Param,
 } from '@nestjs/common';
+
 import { Produto } from '../models/produto.model';
+import { ProdutosService } from 'src/services/produtos.service';
 
 @Controller('produtos')
 export class ProdutosController {
-  private produtos: Produto[] = [
-    new Produto('LIV01', 'Livro TDD e BDD na prática', 29.9),
-    new Produto('LIV02', 'Livro Iniciando com Flutter', 39.9),
-    new Produto('LIV03', 'Inteligência artificial como serviço', 29.9),
-  ];
+  constructor(private produtosService: ProdutosService) {}
 
   @Get()
   obterTodos(): Produto[] {
-    return this.produtos;
+    return this.produtosService.obterTodos();
   }
 
   @Get(':id')
   obterUm(@Param() params): Produto {
-    const id = +params['id'];
-    return this.produtos[id];
+    const id = params['id'];
+    return this.produtosService.obterUm(id);
   }
 
   @Post()
   criar(@Body() produto: Produto): void {
-    produto.id = this.produtos.length + 1;
-    this.produtos.push(produto);
+    produto.id = 100;
+    this.produtosService.criar(produto);
   }
 
   @Put()
   alterar(@Body() produto: Produto): Produto {
-    return produto;
+    return this.produtosService.alterar(produto);
   }
 
   @Delete(':id')
   apagar(@Param() params): void {
     const id = +params['id'];
-    this.produtos.pop();
+    this.produtosService.apagar(id);
   }
 }
